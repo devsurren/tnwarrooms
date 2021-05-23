40737c57e89e01fb7fe536a3aac333b902ac78ff
devSurren <computingwithsurren@gmail.com>
2021-05-23 20:28:52 +0530

    Tn-war-rooms v 1.0.0

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@ import * as contentful from 'contentful';
 
 
 const client =contentful.createClient({
-    space:'7gldz1u57sm0',
-    accessToken:'StfGEvS3gKd1M5pbzPlvpei7ONS0J3oIo28KitpbSdE'
+    space:process.env.REACT_APP_SPACE_ID,
+    accessToken:process.env.REACT_APP_ACCESS_TOKEN
 
 })
 
